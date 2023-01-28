ea6d4faf39c0a08fd2438674ac4cf6c3b350e69d
Yoshifumi-Kitagawa-2 <kitapo3110@gmail.com>
2023-01-28 13:18:30 +0900

    commit

---
+++ philo/srcs/1_arg_check.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   1_arg_check.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yokitaga <yokitaga@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/28 12:48:45 by yokitaga          #+#    #+#             */
+/*   Updated: 2023/01/28 12:53:01 by yokitaga         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/philosopher.h"
+
+bool arg_check(int argc, char **argv)
+{
+    bool arg_check;
+
+    arg_check = false;
+
+    return (arg_check);
+}
