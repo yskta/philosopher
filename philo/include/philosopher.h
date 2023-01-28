b62ee107e6e9c0ab4993c9c307dfd23cc91d1ad8
Yoshifumi-Kitagawa-2 <kitapo3110@gmail.com>
2023-01-28 14:04:38 +0900

    commit

--- philo/include/philosopher.h
+++ philo/include/philosopher.h
@@ -6,7 +6,7 @@
 /*   By: yokitaga <yokitaga@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/28 12:50:23 by yokitaga          #+#    #+#             */
-/*   Updated: 2023/01/28 13:18:27 by yokitaga         ###   ########.fr       */
+/*   Updated: 2023/01/28 13:28:19 by yokitaga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,12 @@
 #define PHILOSOPHER_H
 
 # include <stdbool.h>
+# include <stddef.h>
 
 bool arg_check(int argc, char **argv);
+
+typedef struct s_data{
+
+}t_data;
 
 #endif
--- philo/srcs/1_arg_check.c
+++ philo/srcs/1_arg_check.c
@@ -6,17 +6,45 @@
 /*   By: yokitaga <yokitaga@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/28 12:48:45 by yokitaga          #+#    #+#             */
-/*   Updated: 2023/01/28 12:53:01 by yokitaga         ###   ########.fr       */
+/*   Updated: 2023/01/28 14:03:29 by yokitaga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/philosopher.h"
 
-bool arg_check(int argc, char **argv)
+bool check_each_arg_num_or_not(char *argv)
 {
-    bool arg_check;
+    
+}
 
-    arg_check = false;
+bool check_args_num_or_not(int argc, char **argv)
+{
+    int     i;
+    bool    check_result;
 
-    return (arg_check);
+    i = 1;
+    check_result = true;
+    while (i < argc)
+    {
+        check_result = check_each_arg_num_or_not(argv[i]);
+        if (check_result == false)
+            break;
+        i++;
+    }
+    return (check_result);
+}
+
+bool check_arg(int argc, char **argv)
+{
+    bool check_arg;
+    size_t i;
+
+    check_arg = true;
+    i = 0;
+    if (argc != 5 && argc != 6)
+        check_arg = false;
+    else if (check_args_num_or_not(argc, argv) == false)
+        check_arg = false;
+    else if ()
+    return (check_arg);
 }
