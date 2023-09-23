aef495923d7ad29874f46b037276bef925edcae8
Yoshifumi Kitagawa <kitapo3110@gmail.com>
2023-09-23 15:27:42 +0900

    deletecoment

--- philo/srcs/0_main.c
+++ philo/srcs/0_main.c
@@ -6,7 +6,7 @@
 /*   By: yokitaga <yokitaga@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/25 11:20:54 by yokitaga          #+#    #+#             */
-/*   Updated: 2023/09/18 19:30:52 by yokitaga         ###   ########.fr       */
+/*   Updated: 2023/09/23 15:26:21 by yokitaga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,18 +19,13 @@ int main(int argc, char **argv)
     all_info = (t_info *)malloc(sizeof(t_info));
     if (!all_info)
         return (error_msg("main:malloc error"));
-    //引数 1つ目：哲学者の人数 2つ目：死ぬまでの時間 3つ目:食事を終えるまでの時間 4つ目:睡眠を終えるまでの時間 + 5つ目:食事しないといけない回数(任意)
     if (check_input(argc, argv) == false)
         return (0);
-    //引数を構造体に格納(データの初期設定)
     if (init_info(all_info, argc, argv) == false)
         return (free_all(all_info));
-    //mutexの初期化
     if (init_mutex(all_info) == false)
         return (free_and_destroy_all(all_info));
-    //哲学者の初期化
     init_each_philo(all_info);
-    //スレッド生成、終了待ち、リソース解放
     if (all_about_thread(all_info) == false)
         return (free_and_destroy_all(all_info));
     free_and_destroy_all(all_info);
--- philo/srcs/5_all_about_thread.c
+++ philo/srcs/5_all_about_thread.c
@@ -6,7 +6,7 @@
 /*   By: yokitaga <yokitaga@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/17 15:03:33 by yokitaga          #+#    #+#             */
-/*   Updated: 2023/09/23 15:23:59 by yokitaga         ###   ########.fr       */
+/*   Updated: 2023/09/23 15:27:34 by yokitaga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,6 @@ bool all_about_thread(t_info *all_info)
 	size_t i;
 
 	i = 0;
-	//スレッド生成
 	while (i < all_info->num_of_philo)
 	{
 		if (pthread_create(&(all_info->thread[i]), NULL, &philo_routine, &(all_info->all_philos[i])) != 0)
@@ -26,7 +25,6 @@ bool all_about_thread(t_info *all_info)
 	}
 	if (pthread_create(&all_info->monitor, NULL, &monitor, (void *)all_info) != 0)
 		return (error_msg("all_about_thread:pthread_create error"));
-	//スレッド終了待ち
 	i = 0;
 	while (i < all_info->num_of_philo)
 	{
